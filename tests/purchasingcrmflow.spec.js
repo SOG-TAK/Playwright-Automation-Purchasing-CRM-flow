5436da83ace594c00739d0254ea342b747b6a79a
Soala <soala@digitakumi.com>
2024-09-18 16:36:52 +0100

    Delete skip,  add API call for VRM

--- tests/purchasingcrmflow.spec.js
+++ tests/purchasingcrmflow.spec.js
@@ -1,47 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { HomePage, YourDitailPage, BikeDitailPage, BikeConditionPage, GetLivePricePage,YourAddressPage,DocumentationPage,BodyWorkPage,MechanicsPage,ExtrasPage,UploadPage,ThankYouPage} from '../app/page/index';
 
-/** Test Case 1: Verify Input Blocks Presence
-- Preconditions: None
-- Steps:
-1. Open "https://staging.webuyanybike.com/"
-2. Verify the presence of the "registration" input block
-3. Verify the presence of the "mileage" input block
-- Expected Result: Both input blocks should be present on the page
-
-Test Case 2: Validate Advanced Form Details
-- Preconditions: None
-- Steps:
-1. Open "https://staging.webuyanybike.com/"
-2. Enter "ND18WYK" in the "registration" input block
-3. Enter "1000" in the "mileage" input block
-4. Press the "Value My Bike" button
-5. Verify the "advanced form" is opened
-6. Verify the "registration" is "ND18WYK"
-7. Verify the "mileage" is "1000"
-8. Verify the bike details are correct
-- Expected Result: The advanced form opens with the correct details
-
-Test Case 3: Proceed to Step 2
-- Preconditions: None
-- Steps:
-1. Press the "Value My Bike" button
-2. Verify the URL is "https://mcstaging.abtesting.superbikefactory.co.uk/sell-your-bike/step-2/"
-- Expected Result: The user should be directed to step 2
-
-Test Case 4: Fill Personal Details and Proceed to Step 3
-- Preconditions: User is on step 2
-- Steps:
-1. Choose "Mrs"
-2. Enter "test" in the "first_name" input
-3. Enter "test" in the "last_name" input
-4. Enter "test@gmail.com" in the "email_address" input
-5. Enter "07123456789" in the "phone" input
-6. Press the "Continue" button
-7. Verify the URL is "https://mcstaging.abtesting.superbikefactory.co.uk/sell-your-bike/step-3/"
-- Expected Result: The user should be directed to step 3*/
-
-test.skip('Test Case 1: Verify Input Blocks Presence', async ({ page }) => {
+test('Test Case 1: Verify Input Blocks Presence', async ({ page }) => {
   const homePage = new HomePage(page)
   await homePage.open()
   await homePage.cookiesWindow.clickAcceptCookiesButton()
@@ -50,7 +10,7 @@ test.skip('Test Case 1: Verify Input Blocks Presence', async ({ page }) => {
   await expect (await homePage.enterYourRegField).toHaveAttribute('placeholder', 'Enter Your Reg')
   await expect (await homePage.enterMileageField).toHaveAttribute('placeholder', 'Enter Mileage') 
 });
-test.skip('Test Case 2: Validate Advanced Form Details', async ({ page }) => {
+test('Test Case 2: Validate Advanced Form Details', async ({ page }) => {
 
   const homePage = new HomePage(page)
   await homePage.open()
@@ -75,7 +35,7 @@ test.skip('Test Case 2: Validate Advanced Form Details', async ({ page }) => {
   await expect (await yourDitailPage.detailTitle(6)).toHaveText('Mileage')
   await expect (await yourDitailPage.detailValue(6)).toHaveText('1000 miles')
 });
-test.skip('Test Case 3', async ({ page }) => {
+test('Test Case 3', async ({ page }) => {
   const homePage = new HomePage(page)
   await homePage.open()
   await homePage.cookiesWindow.clickAcceptCookiesButton()
@@ -90,7 +50,7 @@ test.skip('Test Case 3', async ({ page }) => {
 
 
 });
-test.skip('Test Case 4: Fill Personal Details and Proceed to Step 3', async ({ page }) => {
+test('Test Case 4: Fill Personal Details and Proceed to Step 3', async ({ page }) => {
   const homePage = new HomePage(page)
   await homePage.open()
   await homePage.cookiesWindow.clickAcceptCookiesButton()
@@ -104,10 +64,12 @@ test.skip('Test Case 4: Fill Personal Details and Proceed to Step 3', async ({ p
   expect(page.url()).toBe('https://staging.webuyanybike.com/form-2/')
 });
 test('Scenario 5: Fill in Bike Details and Proceed to Step 5', async ({ page }) => {
+  const response = await request.delete('https://magento.staging.superbikefactory.co.uk/api/v1/test')
+  expect(response.status()).toBe(200)
   const homePage = new HomePage(page)
   await homePage.open()
   await homePage.cookiesWindow.clickAcceptCookiesButton()
-  await homePage.setEnterYourRegField('NK07AZZ')
+  await homePage.setEnterYourRegField('V30ALL')
   await homePage.setEnterMileageField('1000')
   await homePage.clickValueMyBikeButton()
 
